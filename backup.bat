13976201eba71af4187b0dfc25f6fc42f381bd8d
BRUNO ROBERTO LOPES RODRIGUES DE ANDRADE <bruno.rlrandrade@senacsp.edu.br>
2024-10-21 16:22:00 -0300

    V.1.0

---
+++ backup.bat
@@ -0,0 +1,6 @@
+mkdir Backup
+cd \Windows
+copy *.txt \Users\bruno.rlrandrade\Documents\script-backup\Backup
+cd System32
+copy *.log \Users\bruno.rlrandrade\Documents\script-backup\Backup
+copy *.txt \Users\bruno.rlrandrade\Documents\script-backup\Backup
